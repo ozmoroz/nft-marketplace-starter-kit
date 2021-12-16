79bffd8c638d288123f0efc33e288231884881e2
Sergey Stadnik <sergey@snowytime.net>
2021-12-16 16:52:11 +1100

    feat: implement transfer and approve functionality

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -15,11 +15,24 @@ contract ERC721 {
         uint256 indexed _tokenId
     );
 
+    /// @dev This emits when the approved address for an NFT is changed or
+    ///  reaffirmed. The zero address indicates there is no approved address.
+    ///  When a Transfer event emits, this also indicates that the approved
+    ///  address for that NFT (if any) is reset to none.
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 indexed _tokenId
+    );
+
     /// Mapping from token id to the owner
     mapping(uint256 => address) private tokenOwner;
 
     /// Mapping from owner to number of owned tokens;
     mapping(address => uint256) private ownedTokensCount;
+
+    /// Maping of tokens to approved addresses
+    mapping(uint256 => address) private tokenApprovals;
 
     /// @notice Count all NFTs assigned to an owner
     /// @dev NFTs assigned to the zero address are considered invalid, and this
@@ -36,7 +49,7 @@ contract ERC721 {
     ///  about them do throw.
     /// @param _tokenId The identifier for an NFT
     /// @return The address of the owner of the NFT
-    function ownerOf(uint256 _tokenId) external view returns (address) {
+    function ownerOf(uint256 _tokenId) public view returns (address) {
         address owner = tokenOwner[_tokenId];
         require(
             owner != address(0),
@@ -76,5 +89,80 @@ contract ERC721 {
         // Emit ERC721 Transfer event
         // Ref: https://eips.ethereum.org/EIPS/eip-721
         emit Transfer(address(0), _to, _tokenId);
+    }
+
+    // this is not safe!
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal {
+        require(
+            _to != address(0),
+            "Error - ERC721 Transfer to the zero address"
+        );
+        require(
+            ownerOf(_tokenId) == _from,
+            "Trying to transfer a token the address does not own!"
+        );
+
+        ownedTokensCount[_from]--;
+        ownedTokensCount[_to]++;
+
+        tokenOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        _transferFrom(_from, _to, _tokenId);
+    }
+
+    /// @notice Change or reaffirm the approved address for an NFT
+    /// @dev The zero address indicates there is no approved address.
+    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
+    ///  operator of the current owner.
+    /// @param _approved The new approved NFT controller
+    /// @param _tokenId The NFT to approve
+    function approve(address _approved, uint256 _tokenId) external payable {
+        // 1. require that the person approving is the owner.
+        // 2. We are approving an address to a token (tokenId).
+        // 3. Require that we can't approve sending tokens to ourselves (tokens of the owner to the current caller)
+        // 4. Update the map of the approval addresses.
+
+        address owner = ownerOf(_tokenId);
+        require(_approved != owner, "Error: Approval to the current owner");
+        require(
+            msg.sender == owner,
+            "Error: Message sender must be an owner of the token."
+        );
+        tokenApprovals[_tokenId] = _approved;
+        emit Approval(owner, _approved, _tokenId);
+    }
+
+    // Is the address an owner or an approver of the token?
+    function isApprovedOrOwner(address _spender, uint256 _tokenId)
+        internal
+        view
+        returns (bool)
+    {
+        require(exists(_tokenId), "Error: Token does not exist.");
+        address owner = ownerOf(_tokenId);
+        return (_spender == owner || _spender == tokenApprovals[_tokenId]);
     }
 }
