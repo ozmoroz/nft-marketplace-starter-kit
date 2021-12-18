16e5548f895b105d628f0ef7e05580f5e21e708d
Sergey Stadnik <sergey@snowytime.net>
2021-12-18 17:07:39 +1100

    feat: implement ERC721Metadata interface

--- src/contracts/ERC721Metadata.sol
+++ src/contracts/ERC721Metadata.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./interfaces/IERC721Metadata.sol";
+
 /// Metadata for ERC-721  Non-Fungible Token Standard
 /// @dev See https://eips.ethereum.org/EIPS/eip-721
-contract ERC721Metadata {
+contract ERC721Metadata is IERC721Metadata {
     string private _name;
     string private _symbol;
 
@@ -13,12 +15,12 @@ contract ERC721Metadata {
     }
 
     /** name getter */
-    function name() external view returns (string memory) {
+    function name() external view override returns (string memory) {
         return _name;
     }
 
     /** sumbol getter */
-    function symbol() external view returns (string memory) {
+    function symbol() external view override returns (string memory) {
         return _symbol;
     }
 }
