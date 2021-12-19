e53881a3170bad4934bfedad461bfd0e755a8b6e
Sergey Stadnik <sergey@snowytime.net>
2021-12-19 19:33:42 +1100

    test: test minting

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -9,13 +9,13 @@ require("chai")
 const KryptoBird = artifacts.require("./KryptoBird");
 
 contract("KryptoBird", (accounts) => {
+  let contract;
+
+  beforeEach(async () => {
+    contract = await KryptoBird.deployed();
+  });
+
   describe("deployment", () => {
-    let contract;
-
-    beforeEach(async () => {
-      contract = await KryptoBird.deployed();
-    });
-
     it("deploy successfully", () => {
       const address = contract.address;
       assert.notEqual(address, "");
@@ -26,6 +26,21 @@ contract("KryptoBird", (accounts) => {
     it("name and symbol match", async () => {
       expect(await contract.name()).to.equal("KryptoBird");
       expect(await contract.symbol()).to.equal("KBIRDZ");
+    });
+  });
+
+  describe("Minting", async () => {
+    it("create a new token", async () => {
+      const mintResult = await contract.mint("https...1");
+      expect((await contract.totalSupply()).toNumber()).to.equal(1);
+      // Verify firing "Transfer" event.
+      // Transfer event fires from address 0 to accounts[0] which is msg.sender
+      const args = mintResult.logs[0].args;
+      expect(Number(args[0])).to.equal(0);
+      expect(args[1]).to.equal(accounts[0]);
+
+      // Contact minting should be rejected if it already exists
+      await contract.mint("https...1").should.be.rejected;
     });
   });
 });
