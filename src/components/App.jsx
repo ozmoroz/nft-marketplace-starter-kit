b06b4a1568eb25d5550286d16f6b4a40a1fc46ab
Sergey Stadnik <sergey@snowytime.net>
2021-12-20 16:25:57 +1100

    feat: show account info in the front-end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,6 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
-    // console.log(accounts);
     this.setState({ account: accounts });
   }
 
@@ -42,7 +41,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>NFT Marketplace</h1>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <div
+            className="navbar-brand col-sm-3 col-md-3 mr-0"
+            style={{ color: "white" }}
+          >
+            Krypto Birdz NFT (Non Fungible Tokens)
+          </div>
+          <ul className="navbar-nav px3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white">{this.state.account}</small>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
