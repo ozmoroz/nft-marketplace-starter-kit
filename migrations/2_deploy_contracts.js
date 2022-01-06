5b9f871b9472f77ade093bf0acd2eb53fed5bb39
Sergey Stadnik <sergey@snowytime.net>
2022-01-06 14:11:58 +1100

    feat: add libraries to migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+const SafeMath = artifacts.require("SafeMath");
+const Counters = artifacts.require("Counters");
 const KryptoBird = artifacts.require("KryptoBird");
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeMath);
+  deployer.deploy(Counters);
+  deployer.link(Counters, KryptoBird);
   deployer.deploy(KryptoBird);
 };
